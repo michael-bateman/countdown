d9bcea89fe624a3b5774e564861ae2013a4750e5
Michael Bateman <info@michaelbateman.ca>
2018-03-07 08:59:44 -0500

    Adds tn countdown

---
+++ tennessee/scripts/main.js
@@ -0,0 +1,23 @@
+function updateWCTime() {
+    now      = new Date();
+    kickoff  = Date.parse("08 Mar 2018 21:00:00 EST");
+    diff = kickoff - now;
+
+    days  = Math.floor( diff / (1000*60*60*24) );
+    hours = Math.floor( diff / (1000*60*60) );
+    mins  = Math.floor( diff / (1000*60) );
+    secs  = Math.floor( diff / 1000 );
+
+    dd = days;
+    hh = hours - days  * 24;
+    mm = mins  - hours * 60;
+    ss = secs  - mins  * 60;
+
+        document.getElementById("countdown")
+            .innerHTML =
+                dd + ' days ' +
+                hh + ' hours ' +
+                mm + ' minutes ' +
+                ss + ' seconds';
+}
+setInterval(updateWCTime, 1000 );
